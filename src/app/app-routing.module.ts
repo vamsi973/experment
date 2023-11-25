aea98eb1c45a46aa18616a88c26e0541d4072a1b
vamsikrishna <danda.vamsikrishna@gmail.com>
2023-11-25 19:00:00 +0530

    remove unused modues and components in commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FourComponent } from './four/four.component';
-import { OneComponent } from './one/one.component';
-import { ThreeComponent } from './three/three.component';
-import { TwoComponent } from './two/two.component';
+import { AuthguadrdGuard } from './_helper/authguadrd.guard';
 import { LoginComponent } from './accounts/login/login.component';
 import { RegisterComponent } from './accounts/register/register.component';
-import { AuthguadrdGuard } from './_helper/authguadrd.guard'
-import { HomepageComponent } from './homepage/homepage.component';
-import { TestComponent } from './test/test.component';
 import { AlertComponent } from './components/alert/alert.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { OneComponent } from './one/one.component';
 import { RedirectComponent } from './redirect/redirect.component';
+
+
 const usersRoutes = () => import('./users/users.routes').then(x => x.USERS_ROUTES);
 const routes: Routes = [
   { path: '', component: HomepageComponent, canActivate: [AuthguadrdGuard] },
   { path: 'r/:randomString', component: RedirectComponent },
   { path: 'users', loadChildren: usersRoutes, canActivate: [AuthguadrdGuard] },
   { path: 'one', component: OneComponent },
-  { path: 'two', component: TwoComponent },
-  { path: 'three', component: ThreeComponent },
-  { path: 'four', component: FourComponent },
   { path: 'account/login', component: LoginComponent },
   { path: 'account/register', component: RegisterComponent },
-  { path: 'test', component: TestComponent },
-  { path: 'module', loadChildren: () => import('./modulename/modulename.module').then(m => m.ModulenameModule) },
   { path: 'alert', component: AlertComponent },
   { path: 'qrCode', loadChildren: () => import('./qr-code/qr-code.module').then(m => m.QrCodeModule), },
-
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { OneComponent } from './one/one.component';
-import { TwoComponent } from './two/two.component';
-import { ThreeComponent } from './three/three.component';
-import { FourComponent } from './four/four.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RegisterComponent } from './accounts/register/register.component';
 import { LoginComponent } from './accounts/login/login.component';
+import { RegisterComponent } from './accounts/register/register.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { Http } from './http.interceptor';
-import { TestComponent } from './test/test.component';
+import { OneComponent } from './one/one.component';
+
 import { AlertComponent } from './components/alert/alert.component';
 import { RedirectComponent } from './redirect/redirect.component';
 
@@ -22,16 +19,11 @@ import { RedirectComponent } from './redirect/redirect.component';
   declarations: [
     AppComponent,
     OneComponent,
-    TwoComponent,
-    ThreeComponent,
-    FourComponent,
     RegisterComponent,
     LoginComponent,
     HomepageComponent,
-    TestComponent,
     AlertComponent,
     RedirectComponent,
-
   ],
   imports: [
     BrowserModule,
